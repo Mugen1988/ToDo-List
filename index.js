666a458cfa0a69ed8a960bd3d8da462e458b2217
Mugen1988 <145986241+Mugen1988@users.noreply.github.com>
2023-10-10 19:53:41 +0200

    UPDATED JS
    Fixed the bug that allowed adding empty item to the list, and added feature that allows adding new item when hitting the Enter key

--- index.js
+++ index.js
@@ -5,30 +5,51 @@ const deleteBtn = document.getElementById("delete-btn")
 const inputField = document.getElementById("input-field")
 const listedItems = document.getElementById("listed-items")
 
-let listsFromLocalStorage = JSON.parse (localStorage.getItem("myList"))
+const listsFromLocalStorage = JSON.parse (localStorage.getItem("myList"))
 
 if (listsFromLocalStorage) {
     myList = listsFromLocalStorage
     renderList()
 }
 
+//DELETE BUTTON (REMOVE ALL ITEMS FROM LIST)
 deleteBtn.addEventListener("click", function() {
     localStorage.clear()
     myList = [];
     renderList()
 })
 
+//ADDING INPUT FIELD VALUE WITH ENTER KEY
+inputField.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && inputField.value.trim() !== "") {
+        myList.push(inputField.value.trim());
+        inputField.value = "";
+        localStorage.setItem("myList", JSON.stringify(myList));
+        renderList();
+        event.preventDefault(); // Prevent the default form submission behavior
+    }
+});
+
+//SAVE THE INPUT VALUE WITH BUTTON CLICK AND DISABLE SAVING EMPTY VALUE
 saveBtn.addEventListener("click", function() {
-    myList.push(inputField.value)
-    inputField.value = ""
-    localStorage.setItem("myList", JSON.stringify(myList))
-    renderList()
+    const inputValue = inputField.value.trim();
+    if (inputValue !== "") {
+        myList.push(inputValue);
+        inputField.value = "";
+        localStorage.setItem("myList", JSON.stringify(myList));
+        renderList();
+    } else {
+        // Handle the case when the input field is empty (you can show an error message or take appropriate action)
+        alert("Input field cannot be empty!");
+    }
 })
 
+//FUNCTION TO RENDER TO-DO LIST
 function renderList() {
     let listItems = ""
     for (let i = 0; i < myList.length; i++) {
-        listItems += `<li>${myList[i]} <span class="remove-icon" data-index="${i}">
+        listItems += `<li>${myList[i]}
+                        <span class="remove-icon" data-index="${i}">
                             <i class="fa fa-trash" aria-hidden="true"></i>
                         </span>
                         <span class ="edit-icon" data-index="${i}">
@@ -38,6 +59,12 @@ function renderList() {
     }
     listedItems.innerHTML = listItems
     
+    editItem()
+    removeItem()
+}
+
+//FUNCTION FOR EDIT ICON (EDIT ADDED ITEMS)
+function editItem() {
     // Attach click event listeners to edit icons
     const editIcons = document.querySelectorAll(".edit-icon");
     editIcons.forEach((icon) => {
@@ -54,7 +81,10 @@ function renderList() {
             }
         });
     });
+}
 
+//FUNCTION FOR REMOVE ICON (REMOVE ADDED ITEM)
+function removeItem() {
     // Attach click event listeners to remove icons
     const removeIcons = document.querySelectorAll(".remove-icon");
     removeIcons.forEach((icon) => {
